c31fb616ec1989dfb1a411c41d4d29262fe2138d
Otter <ignatovad2006@gmail.com>
2025-12-14 11:11:12 +0300

    Add files via upload

---
+++ Source1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+	int num{};
+	std::cout << "Enter a number: " << std::endl;
+	std::cin >> num;
+	std::cout << "You have entered: \n" << num << std::endl;
+
+	return EXIT_SUCCESS;
+}
