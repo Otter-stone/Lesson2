30bb69b8fb01b842dac7ee23c226abf5587f1461
Otter <ignatovad2006@gmail.com>
2025-12-15 11:45:07 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <string> 
+#include <Windows.h>
 
-int main(){
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
 	std::string word{};
-	std::cout << "Enter the word: " << std::endl;
+	std::cout << "Введите слово: ";
 	std::getline(std::cin, word);
-	std::cout << "You have entered: \n" << word << std::endl;
+	std::cout << "Вы ввели: " << word << std::endl;
 
 	return EXIT_SUCCESS;
 }
